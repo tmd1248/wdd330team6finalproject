1a9ee6a86a74fbeff25d50f9f58ed213abdb24bc
karrassphiri <phi21020@byui.edu>
2023-01-30 05:35:53 +0200

    productList semicolon added

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -10,7 +10,7 @@ function productCardTemplate(product) {
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
-  </li>`
+  </li>`;
 }      
 export default class ProductList {
     constructor(category, dataSource, listElement) {
