8072d7c071200b137ff28af8bddc11391b8c5635
karrassphiri <phi21020@byui.edu>
2023-02-10 21:56:34 +0200

    shoppingCart

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,35 +1,32 @@
-import {getLocalStorage} from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
 
-export default class shoppingCart {
-    constructor(key, selector) {
-        this.key = key;
-        this.selector = selector;
-    }
-
-    renderCartContents() {
-        const cartItems = getLocalStorage(this.key);
-        const htmlItems = [cartItems].map((item) => cartItemTemplate(item));
-        document.querySelector(this.selector).innerHTML = htmlItems.join("");
-    }
+  return newItem;
 }
 
-
-  function cartItemTemplate(item) {
-    const newItem = `<li class="cart-card divider">
-    <a href="#">
-      <img
-        src="${item.Image}"
-        alt="${item.Name}"
-      />
-    </a>
-    <a href="../product_pages/index.html?product=${item.Id}" class="cart-card__image">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-
-    return newItem;
+export default class ShoppingCart {
+  constructor(key, parentSelector) {
+    this.key = key;
+    this.parentSelector = parentSelector;
   }
+  renderCartContents() {
+    const cartItems = getLocalStorage(this.key);
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+  }
+}
