41d8914783a064ca4bedf351c4bbf5544966dc9e
huguettekabemba <kab21012@byui.edu>
2023-02-18 14:51:49 +0200

    cart.js error fixed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,8 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
-import ShoppingCart from "./ShoppingCart.mjs";
+import ShoppingCart from "./shoppingCart.mjs";
 
 loadHeaderFooter();
+getLocalStorage();
 
 const cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
