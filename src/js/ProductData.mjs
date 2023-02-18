e08288c43734e2f14e8599ad940301b36d5baa6e
huguettekabemba <kab21012@byui.edu>
2023-02-18 20:07:14 +0200

    productData.mjs url changed

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+const baseURL = "https://wdd330-backend.onrender.com/";
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,10 +8,10 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
+  //constructor(category) {
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
-  }
+  //}
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
