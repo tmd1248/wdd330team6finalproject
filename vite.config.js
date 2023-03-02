beb2b5145dc12481225e857d0d51d19810cf081b
karrassphiri <phi21020@byui.edu>
2023-03-02 11:57:41 +0200

    edit made on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,19 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        checkedout: resolve(__dirname, "src/checkout/success.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        listing: resolve(__dirname, "src/product-listing/index.html"),
+        product1: resolve(
+          __dirname,
+          "src/product_pages/cedar-ridge-rimrock-2.html"
+        ),
+        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
+        product3: resolve(
+          __dirname,
+          "src/product_pages/northface-alpine-3.html"
+        ),
+        product4: resolve(
+          __dirname,
+          "src/product_pages/northface-talus-4.html"
+        ),
       },
     },
   },
