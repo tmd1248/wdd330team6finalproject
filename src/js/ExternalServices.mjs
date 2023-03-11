4a39894acb06b6c6292d970c883d90ea970ec72e
huguettekabemba <kab21012@byui.edu>
2023-03-11 19:11:17 +0200

    team8changes

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -24,4 +24,43 @@ export default class ExternalServices {
     const data = await convertToJson(response);
     return data.Result;
   }
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+  async loginRequest(user) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch(baseURL + "login", options).then(
+      convertToJson
+    );
+    return response.accessToken;
+  }
+  // make a request to the server for the current orders
+  // requires: a valid token
+  // returns: a list of orders
+  async getOrders(token) {
+    const options = {
+      method: "GET",
+      // the server will reject our request if we don't include the Authorization header with a valid token!
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(baseURL + "orders", options).then(
+      convertToJson
+    );
+    return response;
+  }
 }
