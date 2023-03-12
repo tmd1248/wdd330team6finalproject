89410223ba425f345ed7d4d8de2a2a265f16a055
huguettekabemba <kab21012@byui.edu>
2023-03-12 18:38:42 +0200

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         product: resolve(__dirname, "src/product_pages/index.html"),
         listing: resolve(__dirname, "src/product-listing/index.html"),
         admin: resolve(__dirname, "src/admin/index.html"),
+        checkout2: resolve(__dirname, "src/checkout/success.html"),
       },
     },
   },
