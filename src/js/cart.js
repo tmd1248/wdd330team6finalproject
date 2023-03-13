077f5958c1db03426381861e4f3cbceb2b72a307
karrassphiri <phi21020@byui.edu>
2023-03-13 12:45:05 +0200

    cart checked

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,6 @@ import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
-getLocalStorage();
 
 const cart = new ShoppingCart("so-cart", ".product-list");
 cart.init();
