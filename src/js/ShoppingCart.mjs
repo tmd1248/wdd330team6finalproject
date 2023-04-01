15c4fb816db9f45ea666c1cc897a6855f799c5f6
huguettekabemba <kab21012@byui.edu>
2023-04-01 23:52:37 +0200

    com

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -14,7 +14,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  </li>`;
 
   return newItem;
 }
